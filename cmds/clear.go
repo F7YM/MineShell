b0db16a344fff8aabbe28fa4df23f45feddedaf3
float11 <2471825164@qq.com>
2026-04-24 22:23:11 +0800

    清屏命令

---
+++ cmds/clear.go
@@ -0,0 +1,37 @@
+package cmds
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+type ClearCommand struct{}
+
+func init() {
+	Register(&ClearCommand{})
+}
+
+func (c *ClearCommand) Name() string {
+	return "clear"
+}
+
+func (c *ClearCommand) Execute(args []string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
+	return nil
+}
+
+func (c *ClearCommand) Help() string {
+	return "清空屏幕"
+}
